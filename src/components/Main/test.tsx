5e0800134ce16066aa20a504412e857343c6f2a5
Diego Fersan <diegofersan@gmail.com>
2021-04-03 08:57:58 -0300

    set React Test Library

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('sould render the heading', () => {
+    const { container } = render(<Main />)
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <main>
+        <h2>
+          React Avançado
+        </h2>
+      </main>
+    `)
+  })
+})
