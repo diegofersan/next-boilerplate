b372018190b81142ccf959b5f3d0db95570d0b8f
Diego Fersan <diegofersan@gmail.com>
2021-04-03 11:30:08 -0300

    set support to PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,8 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcode icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta name="description" content="A simples boilerplate" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#f2f2f2" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
